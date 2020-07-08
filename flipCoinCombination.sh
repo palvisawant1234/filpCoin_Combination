f74e7755acf37c8f5a3868ebbf623b8e73e3e50b
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-08 13:11:26 +0530

    Modified: fuctionality of flipping a coin multiple times

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,33 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-r=$((RANDOM%2))
-if [ $r -eq 1 ]
-then
-        echo "Heads won"
-else
-        echo "Tails won"
-fi
+echo "Enter number of flips:"
+read n
+
+declare -A dict
+
+dict[H]=0
+dict[T]=0
+
+for((i=0;i<n;i++))
+do
+        r=$((RANDOM%2))
+        if [ $r -eq 0 ]
+	then
+                dict[H]=$((${dict[H]}+1))
+        else
+                dict[T]=$((${dict[T]}+1))
+        fi
+done
+
+echo Number of times head won: ${dict[H]}
+echo Number of times tail won: ${dict[T]}
+
+val=${dict[H]}
+echo -n "Head percentage: "
+headPercent=`echo - | awk '{print '$val' / '$n' * '100' }'`
+echo $headPercent
+
+val=${dict[T]}
+echo -n "Tail percentage: "
+tailPercent=`echo - | awk '{print '$val' / '$n' * '100' }'`
+echo $tailPercent
