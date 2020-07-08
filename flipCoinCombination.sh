0c5e10c9e929e87baf32252fb7a2fc2df0d21a16
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-08 13:35:41 +0530

    Added: fuctionality of doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -7,7 +7,12 @@ declare -A dict
 
 dict[H]=0
 dict[T]=0
+dict[HH]=0
+dict[HT]=0
+dict[TH]=0
+dict[TT]=0
 
+singlet() {
 for((i=0;i<n;i++))
 do
         r=$((RANDOM%2))
@@ -31,3 +36,53 @@ val=${dict[T]}
 echo -n "Tail percentage: "
 tailPercent=`echo - | awk '{print '$val' / '$n' * '100' }'`
 echo $tailPercent
+}
+
+doublet() {
+
+for((i=0;i<n;i++))
+do
+        r=$((RANDOM%4))
+	case "$r" in
+		0)dict[HH]=$((${dict[HH]}+1));;
+		1)dict[HT]=$((${dict[HT]}+1));;
+		2)dict[TH]=$((${dict[TH]}+1));;
+		3)dict[TT]=$((${dict[TT]}+1));;
+	esac
+done
+
+echo Number of times HH won: ${dict[HH]}
+echo Number of times HT won: ${dict[HT]}
+echo Number of times TH won: ${dict[TH]}
+echo Number of times TT won: ${dict[TT]}
+
+val=${dict[HH]}
+echo -n "HH percentage: "
+hhPercent=`echo - | awk '{print '$val' / '$n' * '100' }'`
+echo $hhPercent
+
+val=${dict[HT]}
+echo -n "HT percentage: "
+htPercent=`echo - | awk '{print '$val' / '$n' * '100' }'`
+echo $htPercent
+
+val=${dict[TH]}
+echo -n "TH percentage: "
+thPercent=`echo - | awk '{print '$val' / '$n' * '100' }'`
+echo $thPercent
+
+val=${dict[TT]}
+echo -n "TT percentage: "
+ttPercent=`echo - | awk '{print '$val' / '$n' * '100' }'`
+echo $ttPercent
+}
+
+echo Enter your choice
+echo 1.Singlet
+echo 2.Doublet
+read ch
+case "$ch" in
+        1)singlet;;
+        2)doublet;;
+esac
+
